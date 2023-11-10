80c2e21912daeef58abdf2aed3b43d4a18b9db7e
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-11-10 11:13:18 +0200

    fix drawing rectangles

--- Lab4/src/shapes/RectangleShape.java
+++ Lab4/src/shapes/RectangleShape.java
@@ -34,7 +34,6 @@ public class RectangleShape extends Shape {
         });
 
         root.setOnMouseReleased(event -> {
-            currentRectangle.getStrokeDashArray().clear();
             currentRectangle = null;
         });
     }
